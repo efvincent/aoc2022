678c30b443548b4c6b41de4b37b505c4746e2407
Eric Vincent <eric@efvincent.com>
2022-12-03 14:54:34 -0500

    factor out intersect for future use

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,27 +1,22 @@
 {- DAY03 : https://adventofcode.com/2022/day/3 -}
 module Day03 (sln03A, sln03B, solve03) where
 
-import Util             (getFile)
+import Util             (getFile, intersect)
 import Data.Char        (ord)
 import Data.List.Split  (chunksOf)
 
+{-| Calculate the score as offsets of ascii values -}
 score :: Char -> Int
 score c | c > 'Z'   = ord c - 96
         | otherwise = ord c - 38
-
-{-| Find the intersection between two lists of the same
-    type, where that type has an instance of Eq -}
-intersect :: Eq a => [a] -> [a] -> [a]
-intersect [] = const []
-intersect xs = filter (`elem` xs)
 
 {-| Part A - split each line in half, find the intersection.
     there may be more than one instance of the same Char in 
     the intersection, so take the head, get the score, take the sum -}
 sln03A :: String -> Int
 sln03A =
-  let inHalf s = splitAt (length s `div` 2) s in
-  sum . map (score . head . uncurry intersect . inHalf) . lines
+  let half s = let (l,r) = splitAt (length s `div` 2) s in [l,r] in
+  sum . map (score . head . intersect. half) . lines
 
 {-| Part B - take the list of lines as chunks of 3, determine
     the group score for each chunk, take the sum -}
@@ -33,8 +28,7 @@ sln03B = sum . map grpScore . chunksOf 3 . lines
     with the third line. Again, there may be > 1 of the same char 
     in the intersection, so take the head, and get the score -}
 grpScore :: [String] -> Int
-grpScore [a,b,c] = score . head . intersect a $ intersect b c
-grpScore _ = error "ASSERT FAILED"
+grpScore = score . head . intersect
 
 -- | Applies the solution (either sln03A or sln03B) to the puzzle data
 solve03 :: (String -> Int) -> IO ()
--- src/Util.hs
+++ src/Util.hs
@@ -10,3 +10,20 @@ getFile file = do
   datapath <- getEnv "AOC2022_DATA"
   let fpath = datapath ++ "/" ++ file
   readFile fpath
+
+{-| Finds the intersection between multiple lists -}
+intersect :: Eq a => [[a]] -> [a]
+intersect [] = [] 
+intersect (h:t) =
+  loop h t 
+  where
+    -- Finds the intersection between two lists
+    intr :: Eq a => [a] -> [a] -> [a] 
+    intr [] = const []
+    intr xs = filter (`elem` xs)
+
+    {- Finds the intersection between multiple lists by
+       recursively calling @intr@ on the accumulated 
+       intersection and the next list -}
+    loop acc [] = acc
+    loop acc (h':t') = loop (intr acc h') t'
