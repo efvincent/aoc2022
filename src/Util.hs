636d37820147da82e5c2c0363db896bc40d9e092
Eric Vincent <eric@efvincent.com>
2022-12-04 00:56:36 -0500

    helpers for file read & solving

--- src/Util.hs
+++ src/Util.hs
@@ -1,4 +1,10 @@
-module Util where
+module Util 
+  ( getSample
+  , getPuzzle
+  , solve
+  , sample
+  , intersect 
+  ) where
 import System.Environment (getEnv)
 
 {-| Given a filename gets the contents of that file as a string. 
@@ -27,3 +33,49 @@ intersect (h:t) =
        intersection and the next list -}
     loop acc [] = acc
     loop acc (h':t') = loop (intr acc h') t'
+
+{-| Zero pad an integer out to a certain number of digits -}
+zpad :: Int -> Int -> String
+zpad digits n =
+  let sn = show n in
+  replicate (digits - length sn) '0' ++ sn
+
+{-| Internal solve, takes a boolean to determine if you're
+    solving the sample (True) or the main puzzle (False) -}
+solve' :: Bool -> Int -> (String -> Int) -> IO ()
+solve' smpl d fn = do
+  str <- getTxt smpl d
+  print (fn str)
+
+{-| Gets the text for either the sample or puzzle data for
+    the specified puzzle day. Files must follow the convention
+    described in the documentation of @solve@ -}
+getTxt :: Bool -> Int -> IO String
+getTxt smpl d = do
+  getFile $ "day" ++ zpad 2 d ++ (if smpl then ".sample" else "") ++ ".txt"
+
+{-| Solve the puzzle day indicated by the parameter. The puzzle data
+    file must be in the directory indicated by system variable
+    @AOC2022_DATA@ and follow the naming convention of 
+    @day??.[sample.]txt@, where @??@ is the zero padded day number
+    and [sample.] is included for the sample data but not for the
+    puzzle data. -}
+solve :: Int -> (String -> Int) -> IO ()
+solve = solve' False
+
+{-| Solve the sample data for the day indicated by the parameter. See
+    documentation for @solve@ for information about puzzle file 
+    convention -}
+sample :: Int -> (String -> Int) -> IO ()
+sample = solve' True
+
+{-| Gets the sample data for a particular puzzle day. Intended for
+    use in the repl -}
+getSample:: Int -> IO String
+getSample = getTxt True
+
+{-| Gets the puzzle data for a particular puzzle day. Intended for
+    use in the repl -}
+getPuzzle:: Int -> IO String
+getPuzzle = getTxt False
+
