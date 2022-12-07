362f9ba208d9929c9202bfb7b4db0b5b9c9b670b
Eric Vincent <eric@efvincent.com>
2022-12-07 12:02:21 -0500

    merge day07 docs

--- src/Y2022/Day07.hs
+++ src/Y2022/Day07.hs
@@ -6,25 +6,39 @@ import Data.List.Split (splitOn)
 import qualified Data.Map as M
 import Util (startsWith)
 
+-- | Directory entries is a map from the directory name to the @FSEntry@
 type DirEntries = M.Map String FSEntry
 
+{-| An entry in the file system, either a file with size and name, or a 
+    directory with size and name and a map to the contents (@DirEntries@) -}
 data FSEntry
   = File Int String
   | Dir Int String DirEntries
   deriving (Show, Eq, Ord)
 
+{-| A command is the simulation of a CLI command typed at the terminal 
+    prompt. -}
 data Command
-  = CD String
-  | UP
-  | LS
+  = CD String   -- ^ Change sub-directory 
+  | UP          -- ^ Change to parent directory
+  | LS          -- ^ List the contents of current directory
   deriving (Show, Eq)
 
-data AST
-  = Cmd Command
-  | FSE FSEntry
+{-| represents a unit of syntaxed parsed out of the raw input -}
+data Lexeme
+  = Cmd Command     -- ^ A command typed at the terminal prompt
+  | FSE FSEntry     -- ^ A file system entry output by the simulated OS
   deriving (Show, Eq)
 
-parseLine :: String -> AST
+{-| parse the input string into a list of lexemes -}
+parse :: String -> [Lexeme]
+parse = map parseLine . lines
+
+{-| parse a single line from the input string into a Lexeme. In this
+    syntax, there's a 1:1 relation between lines of input and lexemes. 
+    Note we skip tokenization and translate raw input directly
+    into the lexemes they represent. -}
+parseLine :: String -> Lexeme
 parseLine [] = error "end of input"
 parseLine s
   | s `startsWith` "$ cd .." = Cmd UP
@@ -35,25 +49,12 @@ parseLine s
     let [rawSize, filename] = splitOn " " s in
     FSE (File (read rawSize) filename)
 
-parseInput :: String -> [AST]
-parseInput = map parseLine . lines
-
-addEntries :: DirEntries -> [AST] -> (DirEntries, [AST])
-addEntries de [] = (de,[])
-addEntries de ast@(h:t) =
-  case h of
-    Cmd _ -> (de, ast)
-    FSE subDir@(Dir _ name _) ->
-      case de M.!? name of
-        Just _ -> addEntries de t
-        Nothing -> addEntries (M.insert name subDir de) t
-    FSE file@(File _ name) ->
-      addEntries (M.insert name file de) t
-
-interpret :: [AST] -> FSEntry
+{-| interprets the lexemes into the @FSEntry@ for the root of the file
+    system -> a tree that contains all the information on this file system -}
+interpret :: [Lexeme] -> FSEntry
 interpret = go []
   where
-    go :: [FSEntry] -> [AST] -> FSEntry
+    go :: [FSEntry] -> [Lexeme] -> FSEntry
     go [] [] = error "invariant violation, no root, no commands"
     go [_] ((Cmd UP):_) = error "invariant violation, up command from root"
 
@@ -93,6 +94,30 @@ interpret = go []
       error $ "invariant violation - unhandled interpret:\ndirs: "
         ++ show dirs ++ "\ncmds: " ++ show cmds
 
+{-| parses lexemes into dirctory entries and adds them to the
+    map of directory entries, recurses down the list of
+    lexemes adding entries to sub directories as needed. -}
+addEntries :: DirEntries -> [Lexeme] -> (DirEntries, [Lexeme])
+addEntries de [] = (de,[])
+addEntries de ast@(h:t) =
+  case h of
+    Cmd _ -> (de, ast)
+    FSE subDir@(Dir _ name _) ->
+      case de M.!? name of
+        Just _ -> addEntries de t
+        Nothing -> addEntries (M.insert name subDir de) t
+    FSE file@(File _ name) ->
+      addEntries (M.insert name file de) t
+
+{-| given an @FSEntry@, updates the size of the entry. For files,
+    the size is already in place as a result of parsing, for
+    directories, the sizes of the contents of the directory is 
+    summed, including recursively updating the size of sub dirs.
+    
+    This is done as a second pass over the directory tree because
+    we can't know for sure that we have the complete directory
+    tree for any entry until we've parsed and interpreted 
+    the entire input string -}
 updateSize :: FSEntry -> FSEntry
 updateSize = fst . go
   where
@@ -107,6 +132,8 @@ updateSize = fst . go
     toEntry (e@(Dir _ n _),_) = (n,e)
     toEntry (e@(File _ n) ,_) = (n,e)
 
+{-| Collects the directory sizes as a list, filtering by a predicate
+    that allows us to answer the puzzle questions -}
 dirSizes :: (Int -> Bool) -> FSEntry -> [Int]
 dirSizes predicate root =
   go [] [root]
@@ -117,16 +144,26 @@ dirSizes predicate root =
       | predicate sz = go (sz:acc) (map snd (M.toList entries) ++ t)
       | otherwise    = go acc (map snd (M.toList entries) ++ t)
 
+{-- Solutions ----------------------------------------------------- 
+    
+    Standard wrappers that can be leveraged by @Util.solve@ 
+-------------------------------------------------------------------}
+
+{-| The first part gets the sum of directory sizes that are less than
+    100k size units -}
 sln07A :: String -> String
 sln07A =
-  show . sum . dirSizes (<= 100000) . updateSize . interpret . parseInput
+  show . sum . dirSizes (<= 100000) . updateSize . interpret . parse
 
+{-| The second part finds the smallest single directory that would have
+    to be deleted in order for the file system to reach the target
+    unused space figure -}
 sln07B :: String -> String
 sln07B s =
   let
     totalSpace        = 70000000
     targetUnused      = 30000000
-    root@(Dir sz _ _) = updateSize . interpret . parseInput $ s
+    root@(Dir sz _ _) = updateSize . interpret . parse $ s
     available         = totalSpace - sz
     reqd              = targetUnused - available
     sizes             = dirSizes (const True) root in
