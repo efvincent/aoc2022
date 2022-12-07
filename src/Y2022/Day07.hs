862267cc332171fde70545180bdd2f3289279a2f
Eric Vincent <eric@efvincent.com>
2022-12-07 04:31:16 -0500

    day 7 B, tidy up

--- src/Y2022/Day07.hs
+++ src/Y2022/Day07.hs
@@ -1,12 +1,10 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
+{- DAY07 : https://adventofcode.com/2022/day/7 -}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-module Y2022.Day07 where
+module Y2022.Day07 (sln07A, sln07B) where
 
-import Util (Parts(..), getNums, getSample, getPuzzle, solve)
 import Data.List.Split (splitOn)
 import qualified Data.Map as M
-import Debug.Trace (trace)
+import Util (startsWith)
 
 type DirEntries = M.Map String FSEntry
 
@@ -25,13 +23,6 @@ data AST
   = Cmd Command
   | FSE FSEntry
   deriving (Show, Eq)
-
-startsWith :: Eq a => [a] -> [a] -> Bool
-startsWith target search
-  | length search > length target || null search || null target = False
-  | otherwise =
-    let target' = take (length search) target in
-    search == target'
 
 parseLine :: String -> AST
 parseLine [] = error "end of input"
@@ -53,13 +44,11 @@ addEntries de ast@(h:t) =
   case h of
     Cmd _ -> (de, ast)
     FSE subDir@(Dir _ name _) ->
-      let t' = trace (name ++ " (dir)") t in
       case de M.!? name of
-        Just _ -> addEntries de t'
-        Nothing -> addEntries (M.insert name subDir de) t'
-    FSE file@(File sz name) ->
-      let name' = trace (name ++ " " ++ show sz ++ " (file)") name in
-      addEntries (M.insert name' file de) t
+        Just _ -> addEntries de t
+        Nothing -> addEntries (M.insert name subDir de) t
+    FSE file@(File _ name) ->
+      addEntries (M.insert name file de) t
 
 interpret :: [AST] -> FSEntry
 interpret = go []
@@ -72,9 +61,7 @@ interpret = go []
     go [root] [] = root
 
     -- command to cd when there's no stack
-    go [] ((Cmd (CD subDirName)):t) =
-      let t' = trace ("CD " ++ subDirName ++ " from root") t in
-      go [Dir 0 subDirName M.empty] t'
+    go [] ((Cmd (CD n)):t) = go [Dir 0 n M.empty] t
 
     -- tracking back up the stack, no more commands
     go stack@((Dir _ n _):ancestors) []
@@ -82,33 +69,24 @@ interpret = go []
       | otherwise = go ancestors []
 
     -- command to go up one directory
-    go ((Dir _ name contents):(Dir _ parentName parentContents):ancestors) ((Cmd UP):t) =
-      let t' = trace ("Cmd UP") t in
-      go ((Dir 0 parentName (M.insert name (Dir 0 name contents) parentContents)):ancestors) t'
+    go ((Dir _ n entries):(Dir _ pn pe):ancestors) ((Cmd UP):t) =
+      go (Dir 0 pn (M.insert n (Dir 0 n entries) pe):ancestors) t
 
     -- command to list the contents of the current directory. From the
     -- commands process directory entries (FSE) until you get to the end or
     -- a command Cmd
-    go ((Dir wdSz name contents):ancestors) ((Cmd LS):t) =
-      let
-          t' = trace ("ls from " ++ name) t
-          (contents', cmds) = addEntries contents t' in
-      go (Dir wdSz name (trace (" \\-> new contents of " ++ name ++ ": " ++ show contents') contents'):ancestors) cmds
+    go ((Dir wdSz n entries):ancestors) ((Cmd LS):t) =
+      let (entries', cmds) = addEntries entries t in
+      go (Dir wdSz n entries':ancestors) cmds
 
     -- command to change into a subdirectory
-    go ((Dir wdSz wsName contents):ancestors) ((Cmd (CD subDirName)):t) =
-      -- create or access the child
-      -- add the child to the current directory
-      -- replace working directory in stack
-      -- push subdir to the top of the stack & continue 
-      let
-          subdirName' = trace ("CD " ++ subDirName ++ " from " ++ wsName) subDirName
-          subdir =
-            case contents M.!? subdirName' of
+    go ((Dir dSz dName entries):ancestors) ((Cmd (CD subDirName)):t) =
+      let subdir =
+            case entries M.!? subDirName of
               Just subDir -> subDir
               Nothing -> Dir 0 subDirName M.empty
-          contents' = M.insert subDirName subdir contents
-          newCurDir = Dir wdSz wsName (trace ("new contents of " ++ wsName ++ ": " ++ show contents') contents')
+          entries' = M.insert subDirName subdir entries
+          newCurDir = Dir dSz dName entries'
       in go (subdir:newCurDir:ancestors) t
 
     go dirs cmds =
@@ -116,17 +94,18 @@ interpret = go []
         ++ show dirs ++ "\ncmds: " ++ show cmds
 
 updateSize :: FSEntry -> FSEntry
-updateSize = 
-  fst . go
+updateSize = fst . go
   where
     go :: FSEntry -> (FSEntry,Int)
     go f@(File s _) = (f, s)
     go (Dir _ name contents) =
       let
-        entries = map go . M.elems $ contents
-        contents' = M.fromList $ map (\(e,_) -> case e of (Dir _ n _) -> (n,e); (File _ n) -> (n,e)) entries
-        sz = sum . map snd $ entries
+        entries   = map go . M.elems $ contents
+        contents' = M.fromList . map toEntry $ entries
+        sz        = sum . map snd $ entries
       in (Dir sz name contents', sz)
+    toEntry (e@(Dir _ n _),_) = (n,e)
+    toEntry (e@(File _ n) ,_) = (n,e)
 
 dirSizes :: (Int -> Bool) -> FSEntry -> [Int]
 dirSizes predicate root =
@@ -134,20 +113,21 @@ dirSizes predicate root =
   where
     go acc [] = acc
     go acc ((File _ _):t) = go acc t
-    go acc ((Dir sz n entries):t)
-      | predicate sz =
-          let sz' = trace ("counting " ++ n ++ ": " ++ show sz) sz in
-          go (sz':acc) (map snd (M.toList entries) ++ t)
-      | otherwise =
-          let t' = trace ("skipping " ++ n ++ ": " ++ show sz) t in
-          go acc (map snd (M.toList entries) ++ t')
+    go acc ((Dir sz _ entries):t)
+      | predicate sz = go (sz:acc) (map snd (M.toList entries) ++ t)
+      | otherwise    = go acc (map snd (M.toList entries) ++ t)
 
-sln :: String -> String
-sln =
+sln07A :: String -> String
+sln07A =
   show . sum . dirSizes (<= 100000) . updateSize . interpret . parseInput
 
-totalSpace :: Int
-totalSpace = 70000000
-
-targetUnused :: Int
-targetUnused = 30000000
+sln07B :: String -> String
+sln07B s =
+  let
+    totalSpace        = 70000000
+    targetUnused      = 30000000
+    root@(Dir sz _ _) = updateSize . interpret . parseInput $ s
+    available         = totalSpace - sz
+    reqd              = targetUnused - available
+    sizes             = dirSizes (const True) root in
+  show . minimum . filter (>= reqd) $ sizes
