c6cf91a502cfceb51feee0ab499214041e85a5a2
Eric Vincent <eric@efvincent.com>
2022-12-08 03:21:59 -0500

    organize

--- src/Y2022/Day08.hs
+++ src/Y2022/Day08.hs
@@ -1,32 +1,13 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-module Y2022.Day08 where
+module Y2022.Day08 (sln08A, sln08B) where
 
-import qualified Data.Map as M
 import Data.Vector ((!))
 import qualified Data.Vector as V
 import qualified Data.Set as S
 import Data.List (foldl')
-import Util ( getSample, getPuzzle )
-import Debug.Trace (trace)
-
-samp :: IO String
-samp = getSample 8
-puzz :: IO String
-puzz = getPuzzle 8
 
 type Grid = V.Vector (V.Vector Int)
 type Loc = (Int,Int)
 type Seen = S.Set Loc
-type Scores = M.Map Loc Int
-
-parse :: String -> Grid
-parse = V.fromList . map rowToVec . lines
-  where
-    rowToVec :: String -> V.Vector Int
-    rowToVec = V.fromList . reverse . foldl' (\acc c -> read [c]:acc) []
-
-treeAt :: Loc -> Grid -> Int
-treeAt (x, y) g = (g ! y) ! x
 
 checkRow :: Int -> Grid -> Seen -> Seen
 checkRow y g seen =
@@ -45,7 +26,7 @@ checkRow y g seen =
         else go next checkStop s                  tallest (next x) 
 
 checkCol :: Int -> Grid -> Seen -> Seen
-checkCol x' g seen =
+checkCol x g seen =
   let
     maxY = V.length g
     seen' = go (+ 1) (== maxY) seen (negate 1) 0 in
@@ -55,18 +36,15 @@ checkCol x' g seen =
     go next checkStop s tallest y 
       | checkStop y = s
       | otherwise =
-        let val = treeAt (x',y) g in
+        let val = treeAt (x,y) g in
         if val > tallest
-        then go next checkStop (S.insert (x',y) s) val     (next y)
+        then go next checkStop (S.insert (x,y) s) val     (next y)
         else go next checkStop s                   tallest (next y) 
 
 check :: Grid -> Seen
 check g =
   let seen = foldl' (\acc y -> checkRow y g acc) S.empty [0..(V.length g - 1)] in
   foldl' (\acc x -> checkCol x g acc) seen [0..(V.length (g ! 0) - 1)]
-
-outOfBounds :: Loc -> Grid -> Bool
-outOfBounds (x,y) g = x < 0 || y < 0 || x >= V.length (g ! 0) || y >= V.length g
 
 scenic :: Grid -> Loc -> Int
 scenic g (x,y) =
@@ -85,6 +63,10 @@ scenic g (x,y) =
 allIdx :: Grid -> [Loc]
 allIdx g = [(x,y) | y <- [0..V.length g - 1], x <- [0..V.length (g!0) - 1]]
 
+{-- Solutions ----------------------------------------------------- 
+    Standard wrappers that can be leveraged by @Util.solve@ 
+-------------------------------------------------------------------}
+
 sln08A :: String -> Int
 sln08A = length . check . parse 
 
@@ -92,3 +74,17 @@ sln08B :: String -> Int
 sln08B s =
   let g = parse s in
   maximum . map (scenic g) . allIdx $ g
+
+{-- Helpers -------------------------------------------------------}
+
+parse :: String -> Grid
+parse = V.fromList . map rowToVec . lines
+  where
+    rowToVec :: String -> V.Vector Int
+    rowToVec = V.fromList . reverse . foldl' (\acc c -> read [c]:acc) []
+
+outOfBounds :: Loc -> Grid -> Bool
+outOfBounds (x,y) g = x < 0 || y < 0 || x >= V.length (g ! 0) || y >= V.length g
+
+treeAt :: Loc -> Grid -> Int
+treeAt (x, y) g = (g ! y) ! x
