dc6dbc8ee95119928934e82d4fe53e7d43d04b35
Eric Vincent <eric@efvincent.com>
2022-12-08 23:54:50 -0500

    day 8 refactor wip

--- src/Y2022/Day08.hs
+++ src/Y2022/Day08.hs
@@ -1,13 +1,21 @@
-module Y2022.Day08 (sln08A, sln08B) where
+module Y2022.Day08 
+-- (sln08A, sln08B) 
+where
 
 import Data.Vector ((!))
 import qualified Data.Vector as V
 import qualified Data.Set as S
 import Data.List (foldl')
+import Util (sample, getPuzzle, getSample)
 
 type Grid = V.Vector (V.Vector Int)
 type Loc = (Int,Int)
 type Seen = S.Set Loc
+
+samp :: IO String
+samp = getSample 8 
+puzz :: IO String
+puzz = getPuzzle 8
 
 checkRow :: Int -> Grid -> Seen -> Seen
 checkRow y g seen =
@@ -46,6 +54,23 @@ check g =
   let seen = foldl' (\acc y -> checkRow y g acc) S.empty [0..(V.length g - 1)] in
   foldl' (\acc x -> checkCol x g acc) seen [0..(V.length (g ! 0) - 1)]
 
+checkRC :: Loc -> Grid -> Seen -> Seen
+checkRC (tx,ty) g seen =
+  let seen1 = loop seen  (negate 1) (tx,ty+1) (\(x,y) -> (x,y+1))  {- down  -} 
+      seen2 = loop seen1 (negate 1) (tx,ty-1) (\(x,y) -> (x,y-1))  {- up    -}
+      seen3 = loop seen2 (negate 1) (tx-1,ty) (\(x,y) -> (x-1,y))  {- left  -}
+      seen4 = loop seen3 (negate 1) (tx+1,ty) (\(x,y) -> (x+1,y))  {- right -}
+  in seen4
+  where
+    loop :: Seen -> Int -> Loc -> (Loc -> Loc) -> Seen
+    loop acc tallest loc next
+      | outOfBounds loc g = acc
+      | otherwise =
+        let val = treeAt loc g in
+        if val > tallest
+        then loop (S.insert loc acc) val     (next loc) next
+        else loop acc                tallest (next loc) next
+
 scenic :: Grid -> Loc -> Int
 scenic g (x,y) =
     go 0 (x,y-1) (\(x',y') -> (x'     , y' - 1)) 
@@ -60,9 +85,6 @@ scenic g (x,y) =
         if treeAt loc g >= treeAt (x,y) g then acc + 1
         else go (acc+1) (next loc) next
         
-allIdx :: Grid -> [Loc]
-allIdx g = [(x,y) | y <- [0..V.length g - 1], x <- [0..V.length (g!0) - 1]]
-
 {-- Solutions ----------------------------------------------------- 
     Standard wrappers that can be leveraged by @Util.solve@ 
 -------------------------------------------------------------------}
@@ -88,3 +110,6 @@ outOfBounds (x,y) g = x < 0 || y < 0 || x >= V.length (g ! 0) || y >= V.length g
 
 treeAt :: Loc -> Grid -> Int
 treeAt (x, y) g = (g ! y) ! x
+
+allIdx :: Grid -> [Loc]
+allIdx g = [(x,y) | y <- [0..V.length g - 1], x <- [0..V.length (g!0) - 1]]
