a7bcdd4e62750f026b9c7d0372b97a7d038ebae2
Eric Vincent <eric@efvincent.com>
2022-12-14 00:46:47 -0500

    solve day12, scaffold day13

---
+++ src/Y2022/Day12.hs
@@ -0,0 +1,53 @@
+module Y2022.Day12 (sln12) where
+
+import Grid (Grid, grid, nbors4, assocs, (!))
+import qualified Data.List as List
+import Algorithm.Search (aStar, bfs)
+import GHC.Arr ((//))
+import Util (Parts (..))
+
+type Hill  = Grid Char
+type Point = (Int,Int)
+
+parse :: String -> Hill
+parse = grid . lines
+
+sln12 :: Parts -> String -> Int
+sln12 part s =
+  let g = parse s
+      f = if part == PartA then maybe 0 fst . solveA else maybe 0 length . solveB
+  in  f g
+
+get :: Char -> Hill -> Point
+get c g = case List.find ((== c) . snd) $ assocs g of
+  Just (p, _) -> p
+  Nothing -> error $ "Could not findOne " ++ show c
+
+prepStartEnd :: Hill -> (Hill, Point, Point)
+prepStartEnd g = (g', start, end)
+  where
+    start = get 'S' g
+    end   = get 'E' g
+    g'    = g // [(start, 'a'), (end, 'z')]
+
+solveA :: Hill -> Maybe (Int, [Point])
+solveA og = aStar
+  (\p -> filter (isReachable g p) $ nbors4 g p)
+  (\_ _ -> 1)
+  (dist end)
+  (== end)
+  start
+  where
+    (g, start, end) = prepStartEnd og
+    dist (x1,y1) (x2,y2) = abs (x1 - x2) + (y1 - y2)
+
+solveB :: Hill -> Maybe [(Int,Int)]
+solveB og = bfs
+  (\p -> filter (flip (isReachable g) p) $ nbors4 g p)
+  ((== 'a') . (g !))
+  end
+  where
+    (g, _start, end) = prepStartEnd og
+
+isReachable :: Hill -> Point -> Point -> Bool
+isReachable g p1 p2 = succ (g ! p1) >= g ! p2
