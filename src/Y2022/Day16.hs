9ad5f1f9d83ec5512006d3bf55108e6a5e11b68a
Eric Vincent <eric@efvincent.com>
2022-12-18 15:48:40 -0500

    prep to manages paths differently

--- src/Y2022/Day16.hs
+++ src/Y2022/Day16.hs
@@ -14,6 +14,21 @@ samp = getSample 16
 puzz :: IO String
 puzz = getPuzzle 16
 
+type ValveId = String
+type Cost = Int
+
+data Path = P 
+  { _n1 :: ValveId
+  , _n2 :: ValveId
+  , _cost :: Cost }
+
+instance Eq Path where
+  (==) :: Path -> Path -> Bool
+  P a1 b1 c1 == P a2 b2 c2 = 
+    (c1 == c2) &&
+       (a1 == a2 && b1 == b2) ||
+       (a1 == b2 && b1 == a2) 
+
 data Valve = Valve
   { _valve :: String
   , _flow :: Int
@@ -24,6 +39,15 @@ instance Show Valve where
   show :: Valve -> String
   show Valve{_valve=v, _flow=f, _leadsTo=others} =
     "[Valve:" ++ v ++ "(" ++ show f ++ ") -> (" ++ intercalate "," others ++ ")]"
+
+parse :: String -> [Valve]
+parse = map parseLine . lines
+  where
+    parseLine :: String -> Valve
+    parseLine s =
+      let rate = getNums s
+          valves = getAllTextMatches (s =~ ("[A-Z]{2}" :: String))
+      in Valve {_valve = head valves, _flow = head rate, _leadsTo = tail valves}
 
 toDot :: [Valve] -> String
 toDot vs = 
@@ -37,12 +61,3 @@ toDot vs =
     edge Valve {_valve=v, _leadsTo=others} = v ++ " -> {" ++ intercalate "," others ++ "}" 
     edges :: [Valve] -> String
     edges = intercalate "\n  " . map edge
-
-parse :: String -> [Valve]
-parse = map parseLine . lines
-  where
-    parseLine :: String -> Valve
-    parseLine s =
-      let rate = getNums s
-          valves = getAllTextMatches (s =~ ("[A-Z]{2}" :: String))
-      in Valve {_valve = head valves, _flow = head rate, _leadsTo = tail valves}
