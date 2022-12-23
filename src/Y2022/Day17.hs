ed196ec2e7d45494a5f5aebda3482012d650e9c8
Eric Vincent <eric@efvincent.com>
2022-12-23 11:28:45 -0500

    tidy + comment

--- src/Y2022/Day17.hs
+++ src/Y2022/Day17.hs
@@ -51,22 +51,30 @@ rocks = cycle $ map (moveRock (P 2 0))
 
 {-- Solutions----------------------------------------------------}
 
+{-| calls @placeRock@ iteratively @n@ times and returns the state
+    of the board with rocks placed-}
 sln17 :: Int -> [Jet] -> Board
 sln17 n jets =
   let (_,board',_) = iterate placeRock (rocks, initBoard, jets) !! n
   in board'
 
+{-| pops the next rock off the stack, places it 3 units above the 
+    top of the rock pile, then applies jets and drops until it 
+    settles by calling @jetAndDrop@ -}
 placeRock :: ([Rock],Board, [Jet]) -> ([Rock],Board,[Jet])
 placeRock (rh@(Rock _ height):rt, board, jets) =
-  let r = moveRock (P 0 (maxH board + height + 3)) rh
-      (board', jets') = go r board jets
+  let rock = moveRock (P 0 (maxH board + height + 3)) rh   -- move the rock to 3 above top of puzzle
+      (board', jets') = jetAndDrop rock board jets
   in (rt, board', jets')
-  -- where
-go :: Rock -> Board -> [Jet] -> (Board, [Jet])
-go r@(Rock _ _) b (jh:jt) =
+
+{-| applies a jet (within constraints) then a drop (again within
+    constraints) and returns the new state of the board, and the
+    remaining "jets" -}
+jetAndDrop :: Rock -> Board -> [Jet] -> (Board, [Jet])
+jetAndDrop r@(Rock _ _) b (jh:jt) =
   let r'@(Rock pts _) = jetRock jh r 
   in case dropRock r' b of
-    Just r'' -> go r'' b jt
+    Just r'' -> jetAndDrop r'' b jt
     Nothing -> (S.union b pts, jt)
 
 {-- Helpers -----------------------------------------------------}
@@ -80,6 +88,15 @@ jetRock j r =
       r' = moveRock f r
   in fromMaybe r (checkWall r')
 
+{-| drops a rock down one and returns Just the rock at the new
+    position, or if not possible, returns Nothing -}
+dropRock :: Rock -> Board -> Maybe Rock
+dropRock r b =
+  let r' = moveRock down r
+  in checkBoard r' b
+
+{-| Check if a rock is out of bounds wrt the walls. Return the rock
+    if it is legal, otherwise return Nothing -}
 checkWall :: Rock -> Maybe Rock
 checkWall r@(Rock pts _) =
   let xs = S.map (\(P x _) -> x) pts
@@ -87,49 +104,38 @@ checkWall r@(Rock pts _) =
       maxX = maximum xs
   in if minX >= 0 && maxX <= 6 then Just r else Nothing
 
-{-| returns true if rock does not conflict with the board -}
-checkRock :: Rock -> Board -> Maybe Rock
-checkRock r@(Rock pts _) b =
-  let overlap = S.intersection pts b
-      -- overlap' = if null overlap then overlap else (trace ("intersection:" ++ show overlap ++ " pts:" ++ show pts ++ " board:" ++ show b) overlap)
-  in if null overlap
-     then Just r 
-     else Nothing
-
-{-| slides a rock in the direction of the jet, so long as it's
-    not blocked by one of the pieces on the board -}
--- slide :: Jet -> Rock -> Board -> Rock
--- slide j r b =
---   let r' = jetRock j r 
---   in fromMaybe r $ checkRock r' b
-
-{-| drops a rock down one and returns Just the rock at the new
-    position, or if not possible, returns Nothing -}
-dropRock :: Rock -> Board -> Maybe Rock
-dropRock r b =
-  let r' = moveRock down r
-  in checkRock r' b
+{-| Check if a rock is in conflict with the rock pile. Return the
+    rock if it is legal, otherwise return Nothing -}
+checkBoard :: Rock -> Board -> Maybe Rock
+checkBoard r@(Rock pts _) b =
+  if null (S.intersection pts b) then Just r else Nothing
 
 {-| produces an infinite array of jets -}
 parse :: String -> [Jet]
 parse = let pc '<' = L ; pc '>' = R in cycle . map pc
 
+{-| prints the board to stdio in ascii as presented in AoC -}
 pb :: Board -> IO ()
 pb b =
   let mH = maxH b
-      coords = chunksOf 7 . sortBy (\(P _ y) (P _ y') -> y' `compare` y) $ [P x y | y <- [mH, (mH-1)..1], x <- [0..6]]
-      strs = intercalate "\n" $ map ((\s -> '|':s ++ "|") . map mcoord) coords
-  in putStrLn $ strs ++ "\n+-------+"
+      printCoords = [P x y | y <- [mH, (mH-1)..1], x <- [0..6]]
+      coords      = chunksOf 7 . rowSorter $ printCoords 
+      output      = intercalate "\n" . mkBorders $ coords
+  in putStrLn $ output ++ "\n+-------+"
   where
-    mcoord :: Point -> Char
-    mcoord c =
-      if c `S.member` b then '#' else '.'
+    mkBorders   = map ((\s -> '|':s ++ "|") . map pointChar)
+    rowSorter   = sortBy $ \(P _ y) (P _ y') -> y' `compare` y
+    pointChar c = if c `S.member` b then '#' else '.'
 
+{-| returns the max hieght of the rock pile -}
 maxH :: Board -> Int
 maxH = foldl' (\acc (P _ y) -> if acc < y then y else acc) 0
 
+{-| translates a point by the x y of the other point -}
 move :: Point -> Point -> Point
 move (P x1 y1) (P x2 y2) = P (x1+x2) (y1+y2)
 
+{-| moves a rock by translating all the points by the x y of 
+    the point passed in the first parameter -}
 moveRock :: Point -> Rock -> Rock
 moveRock p (Rock pts n) = Rock (S.map (move p) pts) n
